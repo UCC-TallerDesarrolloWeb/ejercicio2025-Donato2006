09ec91d22d87a5495681f8c4dc67b78e6a0bb05b
Donato Mariano Pallotti <2414596@ucc.edu.ar>
2025-09-08 11:47:05 -0300

    ejercicio js

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,33 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function calcularvalor(unidad,valor){
+    let metro, pie, pulgada, yarda;
+    console.log(valor)
+    console.log(isNaN(valor))
+    if (isNaN(valor)){
+        alert("valor no valido")
+        metro = ""
+        pie = ""
+        yarda = ""
+        pulgada = ""
+    }else if (unidad=="unid_metro"){
+        metro= valor;
+        pie = 3.28 * metro;
+        pulgada=39.37 * metro;
+        yarda=1.0936 * metro;
+    }else if(unidad=="unid_pie"){
+        pie=valor;
+        metro= 0.3048 * pie;
+        pulgada=12 * pie;
+        yarda=0.3333 * pie;
+    }else if(unidad=="unid_pulgada"){
+        pulgada=valor;
+
+    }
+    document.getElementById("metro").value = metro;
+    document.getElementById("pulgada").value = pulgada;
+    document.getElementById("yarda").value = yarda;
+    document.getElementById("pie").value = pie;
+}
